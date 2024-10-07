87f7ab9711085b5662649be33182422ad09e5453
LeJuanTest <juanflow20xdgg@gmail.com>
2024-10-06 22:14:29 -0700

    UI resolution fixed, separated logo redirecting to discord server and members fixed.

--- frontend/assets/js/halloweenBats.js
+++ frontend/assets/js/halloweenBats.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
     let batsHidden = localStorage.getItem('batsHidden') === 'true';
+    let logoNormal = 'assets/img/asd.png'
+    let logoHalloween = 'assets/img/asd2.png'
     let bannerNormal = 'assets/img/BANNER4.png';
     let bannerHalloween = 'assets/img/banner_halloween.png'; 
-    let isHalloweenBanner = localStorage.getItem('isHalloweenBanner'); 
+    let isHalloweenBanner = localStorage.getItem('isHalloweenBanner');
+    let isHalloweenLogo = localStorage.getItem('isHalloweenLogo'); 
 
+
+    if (isHalloweenLogo === null) {
+        isHalloweenLogo = 'false';
+        localStorage.setItem('isHalloweenLogo', 'false');
+    }
     if (isHalloweenBanner === null) {
         isHalloweenBanner = 'false';
         localStorage.setItem('isHalloweenBanner', 'false');
@@ -36,11 +44,24 @@ $(document).ready(function() {
     // Mostrar el banner correcto basado en el estado
     if (isHalloweenBanner === 'true') {
         $('#banner').attr('src', bannerHalloween);
-        $('#halloweenTema').prop('checked', true); // Marcar el toggle si Halloween está activo
+        $('#halloweenTema').prop('checked', true); 
     } else {
         $('#banner').attr('src', bannerNormal);
     }
 
+    if (isHalloweenLogo === 'true') {
+        $('#logo').attr('src', logoHalloween);
+        $('#logo').css({
+            'top': '-6%',
+        });
+        $('#halloweenTema').prop('checked', true); 
+    } else {
+        $('#logo').attr('src', logoNormal);
+        $('#logo').css({
+            'top': '',
+        });
+    }
+    
     // Evento para el botón deslizante (toggle)
     $('#halloweenTema').change(function() {
         if (this.checked) {
@@ -68,6 +89,30 @@ $(document).ready(function() {
                 localStorage.setItem('isHalloweenBanner', 'false');
             }
             $('#banner').css('opacity', '1');
+        }, 300);
+
+        $('#logo').css('opacity', '0'); 
+        setTimeout(function() {
+            let currentLogoSrc = $('#logo').attr('src');
+            if (currentLogoSrc === logoNormal) {
+                $('#logo').attr('src', logoHalloween); 
+                $('#logo').on('load', function() { 
+                    $('#logo').css({
+                        'top': '-6%',
+                        'opacity': '1'  
+                    });
+                    localStorage.setItem('isHalloweenLogo', 'true');
+                });
+            } else {
+                $('#logo').attr('src', logoNormal); 
+                $('#logo').on('load', function() { 
+                    $('#logo').css({
+                        'top': '',
+                        'opacity': '1'  
+                    });
+                    localStorage.setItem('isHalloweenLogo', 'false');
+                });
+            }
         }, 300);
     });
 });
